a7be1ce086dd3662d753db0ea30f45fadfdaac33
rakougit <97489937+rakougit@users.noreply.github.com>
2022-05-09 02:28:30 +0900

    updated Shuzi to Hanzi

--- data_maker_ver5.py
+++ data_maker_ver5.py
@@ -11,6 +11,9 @@ import openpyxl
 from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
 import sys
 import shutil
+import num_to_hanzi_copied
+import warnings
+from decimal import Decimal
 
 
 TS = "出纳账【总帐-东京星银行】"
@@ -19,6 +22,120 @@ M = "出纳账【总帐-瑞穗银行】 "
 BC = "出纳账【总帐-中国银行】"
 Original_data_frame_Column_Names = ["报销单摘要", "支付金额", "附签"]
 Original_data_frame = pd.DataFrame(columns=Original_data_frame_Column_Names)
+
+def cncurrency(value, capital=True, prefix=False, classical=None):
+    '''
+    参数:
+    capital:    True   大写汉字金额
+                False  一般汉字金额
+    classical:  True   元
+                False  圆
+    prefix:     True   以'人民币'开头
+                False, 无开头
+    '''
+    if not isinstance(value, (Decimal, str, int)):
+        msg = '''
+        由于浮点数精度问题，请考虑使用字符串，或者 decimal.Decimal 类。
+        因使用浮点数造成误差而带来的可能风险和损失作者概不负责。
+        '''
+        warnings.warn(msg, UserWarning)
+    # 默认大写金额用圆，一般汉字金额用元
+    if classical is None:
+        classical = True if capital else False
+
+    # 汉字金额前缀
+    if prefix is True:
+        prefix = '人民币'
+    else:
+        prefix = ''
+
+    # 汉字金额字符定义
+    dunit = ('角', '分')
+    if capital:
+        num = ('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖')
+        iunit = [None, '拾', '佰', '仟', '万', '拾', '佰', '仟','亿', '拾', '佰', '仟', '万', '拾', '佰', '仟']
+    else:
+        num = ('〇', '一', '二', '三', '四', '五', '六', '七', '八', '九')
+        iunit = [None, '十', '百', '千', '万', '十', '百', '千','亿', '十', '百', '千', '万', '十', '百', '千']
+    if classical:
+        iunit[0] = '元' if classical else '圆'
+    # 转换为Decimal，并截断多余小数
+
+    if not isinstance(value, Decimal):
+        value = Decimal(value).quantize(Decimal('0.01'))
+
+    # 处理负数
+    if value < 0:
+        prefix += '负'          # 输出前缀，加负
+        value = - value         # 取正数部分，无须过多考虑正负数舍入
+                                # assert - value + value == 0
+    # 转化为字符串
+    s = str(value)
+    if len(s) > 19:
+        raise ValueError('金额太大了，不知道该怎么表达。')
+    istr, dstr = s.split('.')           # 小数部分和整数部分分别处理
+    istr = istr[::-1]                   # 翻转整数部分字符串
+    so = []     # 用于记录转换结果
+
+    # 零
+    if value == 0:
+        return prefix + num[0] + iunit[0]
+    haszero = False     # 用于标记零的使用
+    if dstr == '00':
+        haszero = True  # 如果无小数部分，则标记加过零，避免出现“圆零整”
+
+    # 处理小数部分
+    # 分
+    if dstr[1] != '0':
+        so.append(dunit[1])
+        so.append(num[int(dstr[1])])
+    else:
+        so.append('整')         # 无分，则加“整”
+    # 角
+    if dstr[0] != '0':
+        so.append(dunit[0])
+        so.append(num[int(dstr[0])])
+    elif dstr[1] != '0':
+        so.append(num[0])       # 无角有分，添加“零”
+        haszero = True          # 标记加过零了
+
+    # 无整数部分
+    if istr == '0':
+        if haszero:             # 既然无整数部分，那么去掉角位置上的零
+            so.pop()
+        so.append(prefix)       # 加前缀
+        so.reverse()            # 翻转
+        return ''.join(so)
+
+    # 处理整数部分
+    for i, n in enumerate(istr):
+        n = int(n)
+        if i % 4 == 0:          # 在圆、万、亿等位上，即使是零，也必须有单位
+            if i == 8 and so[-1] == iunit[4]:   # 亿和万之间全部为零的情况
+                so.pop()                        # 去掉万
+            so.append(iunit[i])
+            if n == 0:                          # 处理这些位上为零的情况
+                if not haszero:                 # 如果以前没有加过零
+                    so.insert(-1, num[0])       # 则在单位后面加零
+                    haszero = True              # 标记加过零了
+            else:                               # 处理不为零的情况
+                so.append(num[n])
+                haszero = False                 # 重新开始标记加零的情况
+        else:                                   # 在其他位置上
+            if n != 0:                          # 不为零的情况
+                so.append(iunit[i])
+                so.append(num[n])
+                haszero = False                 # 重新开始标记加零的情况
+            else:                               # 处理为零的情况
+                if not haszero:                 # 如果以前没有加过零
+                    so.append(num[0])
+                    haszero = True
+
+    # 最终结果
+    so.append(prefix)
+    so.reverse()
+    return ''.join(so)
+
 
 
 def OutPutmaker(input_data_frame, outputfile_path, tempfile_path, TargetYear, TargetMonth, ExchangeRate):
@@ -31,9 +148,9 @@ def OutPutmaker(input_data_frame, outputfile_path, tempfile_path, TargetYear, Ta
     wb_target_excel_wb = openpyxl.load_workbook(target_excel_path)
     sheet_name_in_wb_target_excel = ''
     ws = ''
-    print(f'len = {len(input_data_frame)}')
+    # print(f'len = {len(input_data_frame)}')
     for i in range(len(input_data_frame)):
-        print(f'i = {i}')
+        # print(f'i = {i}')
         # Define the input
         # Define the input cell location
         报销单摘要 = "A19"
@@ -41,6 +158,7 @@ def OutPutmaker(input_data_frame, outputfile_path, tempfile_path, TargetYear, Ta
         附签 = "B14"
         汇率 = "G20"
         折合人名币 = "F19"
+        数字转汉字 = "C21"
         copy_flag = "do_not_copy_template_sheet"
         if i % 2 == 0:
             sheet_name_in_wb_target_excel = str(i+1) + "," + str(i+2)
@@ -51,6 +169,7 @@ def OutPutmaker(input_data_frame, outputfile_path, tempfile_path, TargetYear, Ta
             附签 = "B2"
             汇率 = "G8"
             折合人名币 = "F7"
+            数字转汉字 = "C9"
             copy_flag = "copy_template_sheet"
             print(f'i = {i}')
             wb_target_excel_wb.active = int((i/2)+1)
@@ -68,6 +187,7 @@ def OutPutmaker(input_data_frame, outputfile_path, tempfile_path, TargetYear, Ta
             worksheet[折合人名币].value = '\u00A5' + \
                 str(int(worksheet[支付金额].value * float(worksheet[汇率].value)))
             worksheet[支付金额] = '\u00A5' + str(input_data_frame.iloc[i, 1])
+            worksheet[数字转汉字] = cncurrency(worksheet[支付金额].value, capital=True, prefix=False, classical=None)
             wb_target_excel_wb.save(target_excel_path)
             wb_target_excel_wb.close()
         else:
@@ -78,6 +198,7 @@ def OutPutmaker(input_data_frame, outputfile_path, tempfile_path, TargetYear, Ta
             worksheet[折合人名币] = '\u00A5' + \
                 str(int(worksheet[支付金额].value * float(worksheet[汇率].value)))
             worksheet[支付金额] = '\u00A5' + str(input_data_frame.iloc[i, 1])
+            worksheet[数字转汉字] = cncurrency(worksheet[支付金额].value, capital=True, prefix=False, classical=None)
             wb_target_excel_wb.save(target_excel_path)
             wb_target_excel_wb.close()
         # return target_excel_path
